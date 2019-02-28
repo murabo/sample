96228e75bd42f518ede4c6dd3ce089cbaea2adde
村上 進也 <shinya_murakami@murakaminoMac-mini.local>
2019-02-28 22:59:34 +0900

    顧客でも

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
-# from django.contrib.auth.admin import UserAdmin
-# from django.contrib.auth.models import User
-#
-#
-# class UserAdmin(UserAdmin):
-#     pass
-#
-#
-# admin.site.register(User, UserAdmin)
+
+
+from app.models import Client
+
+admin.site.site_title = 'VALUE MANAGEMENT'
+admin.site.site_header = 'VALUE MANAGEMENT'
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'kana', 'mail', 'tel', 'date','link')
+
+    fieldsets = (
+        (None, {'fields': ('first_name', 'last_name')}),
+        (None, {'fields': ('last_name_kana', 'first_name_kana')}),
+        (None, {'fields': ('tel', 'mail')}),
+        (None, {'fields': ('place', 'date')}),
+    )
+
+ClientAdmin.list_display_links = ('name',)
+
+
+admin.site.register(Client, ClientAdmin)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -12,3 +13,32 @@ class Patient(models.Model):
 
     class Meta:
         db_table = 'patient'
+
+
+class Client(models.Model):
+    first_name = models.CharField('姓', max_length=15)
+    last_name = models.CharField('名', max_length=15)
+    first_name_kana = models.CharField('セイ（Last kana）', max_length=30)
+    last_name_kana = models.CharField('メイ（First kana）', max_length=30)
+    tel = models.CharField('電話番号', max_length=15, null=True, default=None)
+    mail = models.CharField('メール', max_length=15, null=True, default=None)
+    place = models.CharField('会場名', max_length=15, null=True, default=None)
+    date = models.DateField('来館予定日', null=True, default=None)
+    link= models.CharField('リンク発行', max_length=15, null=True, default=None)
+    k = models.Transform
+
+
+    def name(self):
+        return self.last_name + self.first_name
+
+
+    def kana(self):
+        return self.last_name_kana + self.first_name_kana
+
+    name.short_description = '氏名'
+    kana.short_description = 'フリガナ'
+
+    class Meta:
+        db_table = 'client'
+        verbose_name='顧客情報'
+        verbose_name_plural = '顧客情報'
